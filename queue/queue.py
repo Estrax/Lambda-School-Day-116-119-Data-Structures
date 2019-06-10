7efe65e59f04e3f19286d35ac4dcf47c3b3bb2d6
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-06-10 15:51:04 -0400

    refactored queue methods to use the given code

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,20 @@
 class Queue:
     def __init__(self):
         self.size = 0
-        # what data structure should we
-        # use to store queue elements?
         self.storage = []
 
     def enqueue(self, item):
         self.storage.append(item)
+        self.size += 1
 
     def dequeue(self):
-        return self.storage.pop(0) if self.len() > 0 else None
+        if not self.is_empty():
+            self.size -= 1
+            return self.storage.pop(0)
+        return None
 
     def len(self):
-        return len(self.storage)
+        return self.size
+
+    def is_empty(self):
+        return self.len() == 0
